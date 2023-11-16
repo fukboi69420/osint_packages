73f7ab0b50b599d4d617a637cf8fd7cfb593ed1f
Tux <82789164+fukboi69420@users.noreply.github.com>
2023-11-16 09:22:53 -0800

    Update osint.sh

--- osint.sh
+++ osint.sh
@@ -13,13 +13,16 @@ git clone https://github.com/nmap/nmap && cd nmap
 ./configure
 make
 make install
+cd
 #install nmap package
 apt-get install -y nmap
 #install ddos-attack
-git clone https://github.com/karthik558/ddos-attack.git && cd ddos-attack
+git clone https://github.com/karthik558/ddos-attack.git
 
+cd osint_package
 #install requirements with both python2 and 3
 pip install -r requirements.lst && pip3 install -r requiremnts.lst
 
 #remove this folder 
-#rm -rf osint_packages
+cd
+rm -rf osint_packages
