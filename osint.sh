4efa6dba64a2ddd2048c8a761cfe1bd32acc8d91
Tux <82789164+fukboi69420@users.noreply.github.com>
2023-11-19 18:33:43 -0800

    added Daprofiler

--- osint.sh
+++ osint.sh
@@ -1,11 +1,16 @@
 #HOME DIR 
 cd
+##update and upograde packages
+apt update
+apt upgrade
+
 ##clone repos
 #clone holehe
 git clone https://github.com/megadose/holehe.git
 cd holehe/
 python3 setup.py install
-cd
+cd ..
+
 #clone maigret
 git clone https://github.com/soxoj/maigret
 #clone nmap 
@@ -13,11 +18,15 @@ git clone https://github.com/nmap/nmap && cd nmap
 ./configure
 make
 make install
-cd
+cd ..
+
 #install nmap package
 apt-get install -y nmap
 #install ddos-attack
 git clone https://github.com/karthik558/ddos-attack.git
+
+##clone and install Daprofiler
+git clone https://github.com/TheRealDalunacrobate/DaProfiler.git
 
 cd osint_package
 #install requirements with both python2 and 3
